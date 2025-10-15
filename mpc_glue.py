a955dc433cc6970906c736fa14da5388ddeff373
LouiseMsn <louise.mansn@gmail.com>
2025-10-15 15:54:11 +0200

    parameters class + custom print

--- mpc_glue.py
+++ mpc_glue.py
@@ -17,11 +17,15 @@ import sys
 
 class MPC():
     def __init__(self, parameters):
-        pass
+        self.robot = ex_robot_data.load()
+        stage_factory = GlueStageFactory()
 
 
 class BaseStageFactory():
     def __init__(self):
+        mpc_parameters = MPCParams()
+        print(mpc_parameters)
+
         self.__stages_model
         pass
     def addJointsLimits(self):
@@ -36,3 +40,69 @@ class BaseStageFactory():
 class GlueStageFactory(BaseStageFactory):
     def __init__(self):
         super().__init__()
+
+# TODO class visu?
+
+class MPCParams():
+    def __init__(self)->None:
+        # Robot
+        self.robot_name : str = "panda"
+        self.world_frame_name : str = "universe"
+        self.start_pose : np.ndarray = np.array([0, 0.0, 0.0, -1, 0.0, 2, 0.0, 0.0, 0.0])
+        self.tool_frame_name : str = "panda_hand_tcp"
+
+        # MPC
+        self.dt : float = 0.01 # time is in seconds
+        self.total_time : int = 1
+        self.n_total_steps : int = int(self.total_time / self.dt)
+
+    def __repr__(self)->str:
+        """
+        Formats the output when printing the object
+        """
+        return f'Robot parameters:\n'\
+                    f'\tRobot name: {self.robot_name}\n'\
+                    f'\tStart pose: {self.start_pose} (rads)\n'\
+                    f'\tWorld frame name: "{self.world_frame_name}"\n'\
+                    f'\tTool frame name: "{self.tool_frame_name}"\n'\
+                f'\nMPC parameters:\n'\
+                    f'\tdt: {self.dt} (secs)\n'\
+                    f'\tTotal time: {self.total_time} (secs)\n'\
+                    f'\tNumber of steps: {self.n_total_steps}\n'\
+
+
+class ProblemParams():
+    def __init__(self)->None:
+        # Weights:
+        self.joint_reg_cost = 1e-2 #1e-4
+        self.vel_reg_cost = 1e-2
+        self.command_reg_cost = 1e-4
+        self.term_state_reg_cost = 1e-4
+        self.waypoint_x_weight = 1e-4
+        self.waypoint_frame_pos_weight = 200.0
+        self.waypoint_frame_vel_weight = 1
+        self.orientation_weight = 50
+
+        # solver
+
+    def __repr__(self):
+        """
+        Formats the output when printing the object
+        """
+        return f'Weights parameters:\n'\
+                f'\tRegulations costs:\n'\
+                    f'\t\tJoints: {self.joint_reg_cost}\n'\
+                    f'\t\tVelocity: {self.vel_reg_cost}\n'\
+                    f'\t\tCommand: {self.command_reg_cost}\n'\
+                    f'\t\tTerminal state: {self.term_state_reg_cost}\n'\
+                f'\n\tWaypoints:\n'\
+                    f'\t\tState: {self.waypoint_x_weight}\n'\
+                    f'\t\tFrame position: {self.waypoint_frame_pos_weight}\n'\
+                    f'\t\tFrame velocity: {self.waypoint_frame_vel_weight}\n'\
+                f'\n\tOrientation: {self.orientation_weight}\n'\
+
+
+
+if __name__ == "__main__":
+    params = MPCParams()
+    print(params)
