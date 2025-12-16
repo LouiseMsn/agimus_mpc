fa0b750abde4e10d42298914d504b9edd880352a
LouiseMsn <louise.mansn@gmail.com>
2025-12-16 11:39:56 +0100

    unlinked visualization to match old version of pinochio used in agimus-demos

--- aligator_mpc/__init__.py
+++ aligator_mpc/__init__.py
@@ -1,4 +1,4 @@
 from aligator_mpc.mpc import MPC
 from aligator_mpc.mpcParameters import Params, args
 from aligator_mpc.mpcTrajectoryUtils import PatternGenerator, TestTrajs
-from aligator_mpc.mpcVisualization import Visualization
+# from aligator_mpc.mpcVisualization import Visualization
