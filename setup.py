782b22a19f88fba0bde7fca4353b580ee2842c5f
LouiseMsn <louise.mansn@gmail.com>
2025-12-16 11:32:50 +0100

    added tap dependency

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="aligator_mpc",
     version="1.0",
     packages=['aligator_mpc'],
+    install_requires = ['typed-argument-parser'],
     py_modules=['mpc', 'mpcParameters', 'mpcTrajectoryUtils', 'mpcVisualization', '']
 ), 
